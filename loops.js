5cf9099fc1b7b967599cf5d0279ec257ae0537a6
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 20:49:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,8 @@
 function forLoop(array) {
-  for(let i = array, )
+  for(i = array; i < array.length; i++) {
+         if (i===0) {  console.log(`I am ${n} strange loop`) }
+          else  { console.log(`I am ${n} strange loops.`) }
+  }
   
   
 }
